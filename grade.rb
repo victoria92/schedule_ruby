f9a19838e54002752816597260c5170fbe41161a
Victoria Hristova <viki.hristova@gmail.com>
2013-02-22 18:21:51 +0200

    add class describing grades

---
+++ grade.rb
@@ -0,0 +1,27 @@
+require './subject'
+
+class Grade
+  attr_accessor :name, :subjects, :program
+
+  def initialize(name, subjects)
+    @name = name
+    @subjects = subjects
+    @program = {}
+  end
+
+  def available(day, hour)
+    program[[day, hour]] == nil ? true : false
+  end
+
+  def available_hours
+    DAYS.map { |day| 1.upto(7).select { |hour| available(day, hour) }.map { |hour| [day, hour] } }.flatten(1)
+  end
+
+  def add_class_to_program(day, hour, teacher)
+    program[[day, hour]] = teacher
+  end
+
+  def remove_class_from_program(day, hour)
+    program.delete [day, hour]
+  end
+end
