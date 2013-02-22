a2ca5c93135e35940c3514f701bb872eeb2fe7fb
Victoria Hristova <viki.hristova@gmail.com>
2013-02-22 18:17:52 +0200

    add class describing teachers

---
+++ teacher.rb
@@ -0,0 +1,31 @@
+DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+class Teacher
+  attr_accessor :name, :subject, :program
+
+  def initialize(name, subject)
+    @name = name
+    @subject = subject #only name!!! Maybe ID?
+    @program = {}
+  end
+
+  def available(day, hour)
+    program[[day, hour]] == nil ? true : false
+  end
+
+  def available_hours
+    DAYS.map { |day| 1.upto(7).select { |hour| available(day, hour) }.map { |hour| [day, hour] } }.flatten(1)
+  end
+
+  def add_class_to_program(day, hour, grade)
+    program[[day, hour]] = grade
+  end
+
+  def remove_class_from_program(day, hour)
+    program.delete [day, hour]
+  end
+
+  def to_s
+    "#{name} #{subject}\n"     #nqkakvo podravnpvane?
+  end
+end
