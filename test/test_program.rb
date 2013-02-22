5de0a7a99c8ce3e84b2f8134ca96498ea3a86551
Victoria Hristova <viki.hristova@gmail.com>
2013-02-22 18:52:28 +0200

    add tests for program

---
+++ test/test_program.rb
@@ -0,0 +1,31 @@
+class ProgramTest < MiniTest::Unit::TestCase
+  def test_creating_empty_program
+    test_program = Program.new
+
+    assert_equal Hash.new, test_program.program
+  end
+
+  def test_adding_class_to_program       #Is it added to grade and teacher's program?
+    test_program = Program.new
+    t = Teacher.new("Boryana Parusheva", "Mathematics")
+    subject = Subject.new("Mathematics", t)
+    grade = Grade.new("9a", [subject])
+    test_class = Classes.parse([grade, subject], ["Monday", 4])
+    test_program.add_class(test_class)
+    result = {["Monday", grade, 4]=>"Mathematics"}
+
+    assert_equal result, test_program.program
+  end
+
+  def test_removing_class_from_program        #Is it removed from grade and teacher's program?
+    test_program = Program.new
+    t = Teacher.new("Boryana Parusheva", "Mathematics")
+    subject = Subject.new("Mathematics", t)
+    grade = Grade.new("9a", [subject])
+    test_class = Classes.parse([grade, subject], ["Monday", 4])
+    test_program.add_class(test_class)
+    test_program.remove_class(test_class)
+
+    assert_equal Hash.new, test_program.program
+  end
+end
