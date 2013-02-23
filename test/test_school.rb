bde7b154bf797aa45b9e5f321f792d45228c1f15
Victoria Hristova <viki.hristova@gmail.com>
2013-02-23 10:25:36 +0200

    Update test/test_school.rb

--- test/test_school.rb
+++ test/test_school.rb
@@ -33,7 +33,14 @@ class SchoolTest < MiniTest::Unit::TestCase
     grade.add_class_to_program("Tuesday", 5, teacher)
     teacher.add_class_to_program("Monday", 6, "9a")
 
-    result = [["Monday", 7], ["Tuesday", 6], ["Tuesday", 7], ["Wednesday", 1], ["Wednesday", 2], ["Wednesday", 3], ["Wednesday", 4], ["Wednesday", 5], ["Wednesday", 6], ["Wednesday", 7], ["Thursday", 1], ["Thursday", 2], ["Thursday", 3], ["Thursday", 4], ["Thursday", 5], ["Thursday", 6], ["Thursday", 7], ["Friday", 1], ["Friday", 2], ["Friday", 3], ["Friday", 4], ["Friday", 5], ["Friday", 6], ["Friday", 7]]
+    result = [["Monday", 7], ["Tuesday", 6], ["Tuesday", 7], ["Wednesday", 1],
+              ["Wednesday", 2], ["Wednesday", 3], ["Wednesday", 4], ["Wednesday", 5],
+              ["Wednesday", 6], ["Wednesday", 7], ["Thursday", 1], ["Thursday", 2],
+              ["Thursday", 3], ["Thursday", 4], ["Thursday", 5], ["Thursday", 6],
+              ["Thursday", 7], ["Friday", 1], ["Friday", 2], ["Friday", 3],
+              ["Friday", 4], ["Friday", 5], ["Friday", 6], ["Friday", 7]
+              ]
+
     assert_equal  result, school.mutual_available_hours(teacher, grade)
   end
 
